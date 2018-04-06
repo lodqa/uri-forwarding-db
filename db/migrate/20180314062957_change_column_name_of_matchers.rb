87643d83936caaf0b3026436b00a29834c8d38c6
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-04-06 22:45:48 +0900

    Fix the wrong column name

--- db/migrate/20180314062957_change_column_name_of_matchers.rb
+++ db/migrate/20180314062957_change_column_name_of_matchers.rb
@@ -1,6 +1,6 @@
 class ChangeColumnNameOfMatchers < ActiveRecord::Migration[5.1]
   def change
-    rename_column :matchers, :xpath_to_media, :xpath_to_media
-    rename_column :matchers, :url_replacement_pattern_for_media, :url_replacement_pattern_for_media
+    rename_column :matchers, :xpath_to_image, :xpath_to_media
+    rename_column :matchers, :url_replacement_pattern_for_image, :url_replacement_pattern_for_media
   end
 end
