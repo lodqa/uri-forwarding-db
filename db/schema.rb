2cfa0b5c8364803546c1e2ef6fc0ef8b02c1d7a2
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-10-05 10:44:17 +0900

    misc: Version up patch version of the Rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314062957) do
+ActiveRecord::Schema.define(version: 2018_03_14_062957) do
 
   create_table "matchers", force: :cascade do |t|
     t.string "uri_pattern", null: false
